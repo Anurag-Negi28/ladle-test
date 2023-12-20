065c5a9836aeaa8607146186aa102a47db593930
mdarslan7 <mdarslan7@gmail.com>
2023-12-20 11:30:25 +0530

    test

--- backend/check.py
+++ backend/check.py
@@ -1,8 +1,15 @@
-def name():
-   return "msg"
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.utils import to_categorical
+import cv2
+import numpy as np
 
-if __name__ == "__main__":
-   print(name())
-
-
-# print(name())  # Add this line to print the result
+def fu():
+   model = models.load_model('digit_recognition_model.h5')
+   custom_image = custom_image = cv2.imread(r'C:\Users\Md. Arslan\OneDrive\Desktop\test\backend\foto.png', cv2.IMREAD_GRAYSCALE)
+   custom_image = cv2.resize(custom_image, (28, 28))
+   custom_image = custom_image.astype('float32') / 255
+   custom_image = custom_image.reshape((1, 28, 28, 1))
+   prediction = model.predict(custom_image)
+   predicted_digit = np.argmax(prediction)
+   return predicted_digit
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const FormData = require('form-data');
+const fs = require('fs');
 const app = express();
 const http = require('http');
 const { Server } = require("socket.io");
+const axios = require('axios');
 const cors = require('cors');
 const { spawn } = require('child_process');
 
@@ -9,38 +12,67 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {origin: "http://localhost:3000", methods: ["GET", "POST"]},
+ cors: {origin: "http://localhost:5173", methods: ["GET", "POST"]},
 });
 
 server.listen(4000, () => { console.log("listening on *:4000"); });
 
-let ladleNumber = null;
-let timeAtLC = null;
-let timeAtConverter = null;
-let activeLadles = 0;
+// let ladleNumber = null;
+// let timeAtLC = null;
+// let timeAtConverter = null;
+// let activeLadles = 0;
 
-// Call Python function
-function callPythonFunction() {
-  const pythonProcess = spawn('python', ['check.py']);
+// io.on('connection', (socket) => {
+//  console.log('client connected: ', socket.id);
 
-  let outputData = '';
+//  socket.on('disconnect', (reason) => {
+//  console.log('client disconnected: ', reason);
+//  });
+// });
 
-  pythonProcess.stdout.on('data', (data) => {
-    outputData += data.toString();
-  });
+io.on('connection', (socket) => {
+    console.log('client connected: ', socket.id);
 
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`Error from Python function: ${data}`);
-  });
+    socket.on('disconnect', (reason) => {
+        console.log('client disconnected: ', reason);
+    });
 
-  pythonProcess.on('close', (code) => {
-    if (code === 0) {
-      console.log(`Python function returned: ${outputData}`);
-    } else {
-      console.error(`Python process exited with code ${code}`);
+    // Add a route to trigger ML model execution
+    
+});
+
+app.get('/predict', async (req, res) => {
+    try {
+        const pythonProcess = spawn('python', ['check.py']);
+        let errorData = '';
+        let result = '';
+ 
+        pythonProcess.stdout.on('data', (data) => {
+            result = data.toString().trim();
+            if (result === 'Error') {
+                // Accumulate error messages
+                errorData += result;
+            }
+        });
+ 
+        pythonProcess.stderr.on('data', (data) => {
+            // Accumulate error messages
+            errorData += data.toString();
+        });
+ 
+        pythonProcess.on('close', (code) => {
+            if (code !== 0 || errorData) {
+                // If there was an error or non-zero exit code
+                console.error('Error:', errorData);
+                res.status(500).send('Internal Server Error');
+            } else {
+                res.send(`Predicted Digit: ${result}`);
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
     }
-  });
-}
+ });
+ 
 
-// Call the Python function when needed
-callPythonFunction();
